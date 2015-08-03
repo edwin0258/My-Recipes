200480843c62d34d92418c5a26e9fe90a87e8cc9
edwin0258 <edwin0258@gmail.com>
2015-08-03 11:26:24 -0700

    Deleted CarrierWave

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,8 +9,6 @@ class Recipe < ActiveRecord::Base
 	validates :name, presence: true, length: {minimum: 5, maximum: 100}
 	validates :summary, presence: true, length: {minimum: 10, maximum: 150}
 	validates :description, presence: true, length: {minimum: 20, maximum: 500}
-	mount_uploader :picture, PictureUploader
-	validate :picture_size
 	default_scope -> { order(updated_at: :desc) }
 
 	def thumbs_up_total
@@ -20,11 +18,4 @@ class Recipe < ActiveRecord::Base
 	def thumbs_down_total
 		self.likes.where(like: false).size
 	end
-
-	private
-		def picture_size
-			if picture.size > 5.megabytes
-				errors.add(:picture, "Should be less than 5MB")
-			end
-		end
 end
